06452cb64fb34279195056bcb1a6c6a819020ace
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-06 02:42:47 +0300

    move food forward

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    //initialize speed of projectile
+    public float speed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move projectile foward (along z axis)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+}
