176fd2ba6a8935a58b37fc05c2a6a3960e3e5f9c
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-06 16:55:37 +0300

    Spawn the animals at timed interval

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,31 +9,42 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPositionZ = 20;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
+        //spawn animal using timer
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //initialize animal index for randomness
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-        //ensure the animals spawn at a random location
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
 
         //method to spawn an animal if s is pressed
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex],spawnPosition , animalPrefabs[animalIndex].transform.rotation);
-        }
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    //call the spawn random animal
+        //    spawnRandomAnimal();
+        //}
 
-
+        
     }
 
 
+    void SpawnRandomAnimal()
+    {
+          //initialize animal index for randomness
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            //ensure the animals spawn at a random location
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+        }
 
 }
